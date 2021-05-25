fecc66de49dc127082d7f63c535865ff0971c4dc
Gabriel Oliveira <65770195+GabrielMeneze@users.noreply.github.com>
2021-05-24 21:10:13 -0300

    text area personalizada

--- dolinkw/src/Pages/CadastroDeVagas/index.jsx
+++ dolinkw/src/Pages/CadastroDeVagas/index.jsx
@@ -37,7 +37,7 @@ const CadastroDeVagas = () => {
     const cadastrar = (event) => {
         event.preventDefault();
 
-        fetch('url' + "o caminho do cadastro vai aqui", {
+        fetch('https://609a8adb0f5a13001721b68b.mockapi.io/api/v1/usuario', {
             method: "POST",
             body: JSON.stringify({
                 titulo: titulo,
@@ -93,46 +93,61 @@ const CadastroDeVagas = () => {
 
                     <form className="form-cupom" onSubmit={cadastrar}>
                         <div className="form-group">
-                            <input
-                                type="text"
-                                name="titulo"
-                                value={titulo}
-                                className="form-control"
-                                placeholder="Titulo"
-                            >
-                            </input>
-                            <input
-                                type="text"
-                                name="faixasalarial"
-                                value={faixasalarial}
-                                className="form-control"
-                                placeholder="Faixa salarial"
-                            >
-                            </input>
-                            <input
-                                type="text"
-                                name="local"
-                                value={local}
-                                className="form-control"
-                                placeholder="Local"
-                            >
-                            </input>
-                            <input
-                                type="text"
-                                name="descricao"
-                                value={descricao}
-                                className="form-control"
-                                placeholder="Descrição da vaga"
-                            >
-                            </input>
-                            <input
-                                type="text"
-                                name="beneficios"
-                                value={beneficios}
-                                className="form-control"
-                                placeholder="Beneficios"
-                            >
-                            </input>
+                            <div className="textarea">
+                                <input
+                                    type="text"
+                                    name="titulo"
+                                    value={titulo}
+                                    className="form-control"
+                                    placeholder="Titulo"
+                                    onChange={(event) => setTitulo(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div className="textarea">
+                                <input
+                                    type="text"
+                                    name="faixasalarial"
+                                    value={faixasalarial}
+                                    className="form-control"
+                                    placeholder="Faixa Salarial"
+                                    onChange={(event) => setFaixasalarial(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div className="textarea">
+                                <input
+                                    type="text"
+                                    name="local"
+                                    value={local}
+                                    className="form-control"
+                                    placeholder="Local"
+                                    onChange={(event) => setLocal(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div className="textarea">
+                                <input
+                                    type="text"
+                                    name="descricao"
+                                    value={descricao}
+                                    className="form-control"
+                                    placeholder="Descrição da vaga"
+                                    onChange={(event) => setDescricao(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div className="textarea">
+                                <input
+                                    type="text"
+                                    name="beneficios"
+                                    value={beneficios}
+                                    className="form-control"
+                                    placeholder="Beneficios"
+                                    onChange={(event) => setBeneficios(event.target.value)}
+                                >
+                                </input>
+                            </div>
                             <button
                                 type="submit"
                                 value="vcupom"
@@ -154,7 +169,7 @@ const CadastroDeVagas = () => {
                         <button class="button" onClick={handleClick}>Upload</button>
                     </div>
 
-                    
+
                     <input type="search" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..." />
                     <datalist id="datalistOptions">
                         <option value="C#" />
