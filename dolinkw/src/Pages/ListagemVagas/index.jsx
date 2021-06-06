ceb3f5e6678cda66ffd0e02db21e6521377d1f03
Gabriel Oliveira <65770195+GabrielMeneze@users.noreply.github.com>
2021-06-06 01:26:43 -0300

    inicio listagem vagas

--- dolinkw/src/Pages/ListagemVagas/index.jsx
+++ dolinkw/src/Pages/ListagemVagas/index.jsx
@@ -4,14 +4,34 @@ import Rodape from '../../components/footer';
 
 const ListagemVagas = () => {
 
+    const cadastrar = (event) => {
+        event.preventDefault();
+
+        fetch('https://localhost:44348/v1/vagancy/create', {
+            method: "LIST",
+            body: JSON.stringify({
+
+            }),
+            headers: {
+                "content-type": "application/json",
+            },
+        })
+            .then((response) => {
+                // Verifica se a validação for OK e caso seja, informa a resposta
+                if (response.ok) {
+                    console.log(response.json());
+                    alert('Vaga Cadastrada!');
+                }
+                // Caso validação não seja OK informa um alert
+                alert("Dado inválido");
+            })
+            .catch((err) => console.error(err));
+    };
+
     return(
-
-        <div>
-            <Header />
-            <h1>Listagem de Vagas</h1>
-            <Rodape />
+        <div className="main">
+            <h1>teste</h1>
         </div>
-
     )
 
 }
