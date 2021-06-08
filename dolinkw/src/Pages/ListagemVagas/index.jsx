126d8e529cfe01a95d3a60f0658b7a160ec7f8e4
DharoOficial <erickbandeiradharo@gmail.com>
2021-06-08 19:32:02 -0300

    estilizacao da pagina listagem de vagas

--- dolinkw/src/Pages/ListagemVagas/index.jsx
+++ dolinkw/src/Pages/ListagemVagas/index.jsx
@@ -3,10 +3,10 @@ import './index.css'
 import Header from '../../components/header';
 import Rodape from '../../components/footer';
 import { Table, Button, Card } from 'react-bootstrap';
-import {  Link  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import empresaServico from '../../servicos/empresaServico';
-import {  useFormik  } from 'formik';
+import { useFormik } from 'formik';
 
 const ListagemVagas = () => {
 
@@ -16,7 +16,7 @@ const ListagemVagas = () => {
     const [empresa, setEmpresa] = useState('');
     const [vagas, setVagas] = useState([]);
 
-    const token = localStorage.getItem('token-dolink');  
+    const token = localStorage.getItem('token-dolink');
 
     useEffect(() => {
 
@@ -26,55 +26,55 @@ const ListagemVagas = () => {
 
     const listarVagas = () => {
         empresaServico
-        .listarvagas()
-        .then(resultado =>{
-            setVagas(resultado.data.data);
-        })
-        .catch(erro =>{
-            console.error(`erro ${erro}`);
-        })
+            .listarvagas()
+            .then(resultado => {
+                setVagas(resultado.data.data);
+            })
+            .catch(erro => {
+                console.error(`erro ${erro}`);
+            })
+
     }
 
 
-    return(
+    return (
 
-        <div>
+        <div className="englobatudoNaPaginiaListagemDeVagas">
             <Header />
-                <div className="sectionPerfilEmpresaAltura">
+            <main>
+                <div className="estilizacaoDePaginaListagemVaga">
 
-                    <div className="sectionPerfilEmpresaLargura">
+                    <div className="FiltoDeVagasListagem">
+                    </div>
 
-                    <Table className="tabelaVagasEmpresa">
+                    <div className="sectionDeCardsDasVagas">
 
-                            <tr className="topicosVagas">
-                                
-                                <th>Título</th>
-                                <th>Faixa Salarial</th>
-                                <th>Local</th>
-
-                            </tr>
-
-                        <tbody>
                         {
                             vagas.filter(item => jwtDecode(token).Id === item.empresa).map((item, index) => {
                                 return (
-                                        <tr key={index}>
+                                    <div >
 
-                                            <td>{item.titulo}</td>
-                                            <td>{item.faixaSalarial}</td>
-                                            <td>{item.local}</td>
-                                            
-                                            <td>
-                                                <Button className="botaoDetalhesVaga" ><Link to={{ pathname : '/ListagemVagaEspecifica', state : {IdVaga : item.id} }}>Detalhes</Link></Button>                                               
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </Table>
+                                        <a className="LinkDeCardsDeVagaListagemdeVagas" src="/ListagemVagaEspecifica" key={index}>
+                                            <div className="cardsDeVagas">
+                                                <div className="cardiparaEstilizacaoDeListagemDeVaga">
+                                                    <p className="TituloCardaVagas">{item.titulo}</p>
+                                                    <p style={{ 'margin-bottom': '0.6em', 'maxWidth' : '95%'  }}>Descrição : {item.descricao}</p>
+                                                    <p style={{ 'margin-bottom': '0.6em' }}>Local : {item.local}</p>
+                                                    <div className="localeFaixaSalarialCard">
+                                                        <p style={{ 'margin-bottom': '0.6em' }}>Faixa Salarial R$: {item.faixaSalarial}</p>
+                                                        <p style={{ 'margin-bottom': '0.6em', 'maxWidth' : '9em' }}>Data Validade : {item.dataVencimento}</p>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
+            </main>
             <Rodape />
         </div>
 
