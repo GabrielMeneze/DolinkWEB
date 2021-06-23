2ffaa199c504e0d9341d206392536c02f5752f9a
Gabriel Oliveira <65770195+GabrielMeneze@users.noreply.github.com>
2021-06-23 18:29:10 -0300

    p

---
+++ dolinkw/src/Pages/chat/index.jsx
@@ -0,0 +1,60 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDVK7pECqLUFH6PywHU536dwp6fbpMsdQc",
+  authDomain: "chatdolink-392ee.firebaseapp.com",
+  projectId: "chatdolink-392ee",
+  storageBucket: "chatdolink-392ee.appspot.com",
+  messagingSenderId: "498776411041",
+  appId: "1:498776411041:web:1aafdc490224afaba299e0",
+  measurementId: "G-9VPG3XF8DC"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const [user] = useAuthState(auth);
+
+
+function SignIn(params) {
+    const SignInWithGoogle = () =>{
+
+    }
+
+    return(
+        <button onClick={SignInWithGoogle}></button>
+    )
+}
+
+function SignOut() {
+
+    return auth.currentUser &&(
+        <button onClick={() => auth.signOut()}>SignOut</button>
+    )
+
+}
+
+function ChatRoom() {
+    const messagesRef = firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+}
+
+return(
+    <div className="app">
+        <header>
+
+        </header>
+
+        <section>
+            {user ? <ChatRoom/> : <SignIn/> }
+        </section>
+    </div>
+)
