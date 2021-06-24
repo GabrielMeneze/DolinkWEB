16de7bccc7d489918b64fc7a818e1b6fd6b58e0d
Gabriel Oliveira <65770195+GabrielMeneze@users.noreply.github.com>
2021-06-24 03:06:46 -0300

    scroll adicionado, necessario teste

--- dolinkw/src/Pages/MatchProfissional/index.jsx
+++ dolinkw/src/Pages/MatchProfissional/index.jsx
@@ -20,7 +20,7 @@ const MatchProfissional = () => {
     const [local, setLocal] = useState('');
     const [faixaSalarial, setFaixaSalarial] = useState('');
 
-    const token = localStorage.getItem('token-dolink');  
+    const token = localStorage.getItem('token-dolink');
 
     const idProfissional = jwtDecode(token).Id;
 
@@ -28,98 +28,98 @@ const MatchProfissional = () => {
         listarPreMatch()
     }, [])
 
-    
+
 
     const listarPreMatch = () => {
         http.get('https://localhost:44338/v1/vagancy/prematch/' + idProfissional, {
-            method : 'GET',
-            
-            body: JSON.stringify ({
+            method: 'GET',
 
-                idVaga : idVaga
-                
+            body: JSON.stringify({
+
+                idVaga: idVaga
+
             })
 
         })
-        .then(resultado =>{
-            setVagas(resultado.data.data);
-            console.log(resultado.data.data)
-        })
-        .catch((err) => console.log(err))
+            .then(resultado => {
+                setVagas(resultado.data.data);
+                console.log(resultado.data.data)
+            })
+            .catch((err) => console.log(err))
     }
 
     const darMatch = (event, id) => {
         event.preventDefault();
 
-        fetch('https://localhost:44338/v1/match' , {
-            method : 'POST',
-            body : JSON.stringify({
+        fetch('https://localhost:44338/v1/match', {
+            method: 'POST',
+            body: JSON.stringify({
 
-                idProfissional : idProfissional,
-                idVaga : id
+                idProfissional: idProfissional,
+                idVaga: id
 
             }),
             headers: {
-                'content-type' : 'application/json'
+                'content-type': 'application/json'
             },
         })
             .then(resultado => resultado.json())
             .then((resultado) => {
 
-                if(resultado.sucesso) {
-                    addToast(resultado.mensagem, { appearance: 'success', autoDismiss : true })
+                if (resultado.sucesso) {
+                    addToast(resultado.mensagem, { appearance: 'success', autoDismiss: true })
                 } else {
-                    addToast(resultado.mensagem, { appearance: 'error', autoDismiss : true })
+                    addToast(resultado.mensagem, { appearance: 'error', autoDismiss: true })
                 }
 
             })
-                .catch((err) => console.error(err));
+            .catch((err) => console.error(err));
     }
 
-    return(
+    return (
 
         <div>
             <Header />
-                
-                <div className="titulo">
-                    <hr className="linha" />
-                    <div className="esp"></div>
-                    <h1>Matchs</h1>
-                    <div className="esp"></div>
-                    <hr className="linha" />
-                </div>
 
-                <a className="linkMatchsConfirmados" href="/matchConfirmadoProfissional">Ver Matchs Confirmados</a>
+            <div className="titulo">
+                <hr className="linha" />
+                <div className="esp"></div>
+                <h1>Matchs</h1>
+                <div className="esp"></div>
+                <hr className="linha" />
+            </div>
 
-                <main>
+            <a className="linkMatchsConfirmados" href="/matchConfirmadoProfissional">Ver Matchs Confirmados</a>
+
+            <main>
                 <div className="estilizacaoDePaginaListagemMatch">
 
-                {
-                            vagas.map((item, index) => {
-                                return (
-                                    <div >
+                    {
+                        vagas.map((item, index) => {
+                            return (
+                                <div >
 
-                                        <div className="LinkDeCardListagemdeMatch" >
-                                            <div className="cardsDeMatch">
-                                                <div className="cardiparaEstilizacaoDeListagemDeMatch">
-                                                    <p className="TituloCardaMatch">{item.titulo}</p>
-                                                    <p style={{ 'margin-bottom': '0.6em', 'maxWidth' : '95%'  }}>{item.descricao}</p>
-                                                    <p style={{ 'margin-bottom': '0.6em' }}>Faixa Salarial:</p>
-                                                    <p style={{ 'margin-bottom': '0.6em' }}>R${item.faixaSalarial}</p>
-                                                    
-                                                    <button onClick={e => darMatch(e, item.id)} className="botaoDarMatch"  type="submit">Dar Match!</button>
-                                                    
-                                                    {/* <button onClick={console.log(item.id)} className="botaoDarMatch" type="button" >Dar Match!</button> */}
+                                    <div className="LinkDeCardListagemdeMatch" >
+                                        <div className="cardsDeMatch">
+                                            <div className="cardiparaEstilizacaoDeListagemDeMatch">
+                                                <p className="TituloCardaMatch">{item.titulo}</p>
+                                                <p style={{ 'margin-bottom': '0.6em', 'maxWidth': '95%' }}>{item.descricao}</p>
+                                                <p style={{ 'margin-bottom': '0.6em' }}>Faixa Salarial:</p>
+                                                <p style={{ 'margin-bottom': '0.6em' }}>R${item.faixaSalarial}</p>
 
-                                                </div>
+                                                <button onClick={e => darMatch(e, item.id)} className="botaoDarMatch" type="submit">Dar Match!</button>
+
+                                                {/* <button onClick={console.log(item.id)} className="botaoDarMatch" type="button" >Dar Match!</button> */}
+
                                             </div>
                                         </div>
                                     </div>
-                                )
-                            })
-                        }
+                                </div>
+                            )
+                        })
+                    }
 
-                    
+
                 </div>
             </main>
 
